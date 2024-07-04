16bb8fc0dc9cb0c898220ae7267896462908165e
Moshfiqur Rahman <studentemam@gmail.com>
2024-07-04 13:46:48 +0600

    create Navbar with logo,navlinks and get quote button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
+import TopNavbar from "./components/TopNavbar";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Fragment>
+      <TopNavbar />
       <Navbar />
     </Fragment>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,70 +1,62 @@
 import { Fragment } from "react";
-import { FaPhoneAlt } from "react-icons/fa";
-import { FaLocationDot } from "react-icons/fa6";
-import { IoMdMail } from "react-icons/io";
-import { FaFacebook } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
+import Logo from "../assets/logo/logo.png"
 
-export default function Navbar() {
-  return (
-    <Fragment>
-      <div className="bg-[#0E121D] w-full p-3">
-        <div className="max-w-screen-xl mx-auto px-8 md:px-16 lg:px-8  flex justify-center md:justify-between ">
-          {/* Start address  */}
-          <div className="hidden md:flex items-center gap-8 xl:gap-10 ">
-            <div className="hidden xl:block">
-              <div className="flex items-center gap-1">
-                <FaPhoneAlt className="text-[#F68812] text-sm" />
-                <span className="text-[#908C8D]  text-sm font-[archivo] ">
-                  +1 201-380-2737
-                </span>
-              </div>
-            </div>
-            <div className="flex items-center gap-1">
-              <FaLocationDot className="text-[#F68812] text-sm" />
-              <span className="text-[#908C8D] ml-1 text-sm font-[archivo]">
-                138 MacArthur Ave, United States
-              </span>
-            </div>
-            <div className="flex items-center gap-1">
-              <IoMdMail className="text-[#F68812] text-sm" />
-              <a
-                href="mailto:studentemam@gmail.com"
-                className="text-[#908C8D] ml-1 text-sm font-[archivo] hover:text-[#F68812] transform transition-all duration-300"
-              >
-                help24/7info@kotar.com
-              </a>
-            </div>
-          </div>
-          {/* Ends address  */}
+export default function Navbar(){
+    return(
+        <Fragment>
+            <div className="bg-[#FFEBDE] w-full ">
+            <div className="max-w-screen-xl mx-auto px-8 md:px-16 lg:px-8 flex justify-between items-center ">
 
-          {/* Start social icon  */}
-          <div className="flex items-center gap-3 xl:gap-5">
-            <span className="text-sm font-[archivo] text-[#908C8D] hidden xl:block">
-              Follow Us:
-            </span>
-            <a href="#">
-              <FaFacebook className="text-sm text-[#908C8D] hover:text-[#1877F2] transform transition-all duration-300" />
-            </a>
-            <a href="#">
-              <FaTwitter className=" text-sm text-[#908C8D] hover:text-[#1DA1F2] transform transition-all duration-300" />
-            </a>
-            <a href="#">
-              <FaLinkedin className="text-sm text-[#908C8D] hover:text-[#0077B5] transform transition-all duration-300" />
-            </a>
-            <a href="#">
-              <FaInstagram className="text-sm text-[#908C8D] hover:text-[#e56969] transform transition-all duration-300" />
-            </a>
-            <a href="#">
-              <FaYoutube className="fa-brands fa-youtube text-sm text-[#908C8D] hover:text-[#FF0000] transform transition-all duration-300" />
-            </a>
-          </div>
-          {/* Ends social icon  */}
+                <div className="flex items-center">
+                     {/* Start logo */}
+                    <div>
+                        <img className=" w-[120px] lg:w-[208px] h-[70px] lg:h-[100px] object-contain" src={Logo} alt="" />
+                        {/* <img src={logo} alt="" /> */}
+                    </div>
+                    {/* Ends logo  */}
+
+                     {/* navlink */}
+                    <div
+                        className="z-10 nav-links transition-all duration-700  lg:static absolute bg-white lg:bg-transparent lg:min-h-fit min-h-[50vh] left-0 top-[-100%] lg:w-auto  w-full flex items-center px-5">
+                        <ul className="flex flex-col lg:flex-row lg:items-center  gap-10">
+                            <li
+                                className="text-sm font-[titillium] text-[#4C4D4E] hover:text-[#F68A0A] transform transition-all duration-200 font-[600]">
+                                <a href="./index.html">HOME</a>
+                            </li>
+                            <li
+                                className="text-sm font-[titillium] text-[#4C4D4E] hover:text-[#F68A0A] transform transition-all duration-200 font-[600]">
+                                <a href="./about.html">ABOUT US</a>
+                            </li>
+                            <li
+                                className="text-sm font-[titillium] text-[#4C4D4E] hover:text-[#F68A0A] transform transition-all duration-200 font-[600]">
+                                <a href="./service.html">SERVICE</a>
+                            </li>
+                            <li
+                                className="text-sm font-[titillium] text-[#4C4D4E] hover:text-[#F68A0A] transform transition-all duration-200 font-[600]">
+                                <a href="./blog.html">BLOG</a>
+                            </li>
+                            <li
+                                className="text-sm font-[titillium] text-[#4C4D4E] hover:text-[#F68A0A] transform transition-all duration-200 font-[600]">
+                                <a href="./contactUs.html">CONTACT US</a>
+                            </li>
+                        </ul>
+                    </div>
+                     {/* Ends navlink */}
+                </div>
+
+                {/* Start get a quote button */}
+                <div className="flex items-center gap-3">
+                    <button
+                        className="bg-[#F68A0A] hover:bg-[#ee8404] transform transition-all duration-300 text-white text-sm font-[archivo] font-[500] px-3 sm:px-7 py-2 sm:py-4 flex items-center gap-1">
+                        GET A QUOTE
+                        <i className="fa-solid fa-arrow-right-to-bracket hidden md:block"></i>
+                    </button>
+                    <ion-icon onclick="onToggleMenu(this)" name="menu"
+                        class="text-3xl cursor-pointer lg:hidden"></ion-icon>
+                </div>
+                 {/* Ends get a quote button */}
+            </div>
         </div>
-      </div>
-    </Fragment>
-  );
+        </Fragment>
+    )
 }
