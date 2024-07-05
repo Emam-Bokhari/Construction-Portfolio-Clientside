e2cb9a293d0a076da4c5b76be7b1f64ba035bfea
Moshfiqur Rahman <studentemam@gmail.com>
2024-07-05 10:41:43 +0600

    create service page and refector service section

--- src/components/Page/Home/Home.jsx
+++ src/components/Page/Home/Home.jsx
@@ -1,26 +1,29 @@
 import { Fragment } from "react";
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import {
+  FaArrowRight,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
 import whyChooseUsImage from "../../../assets/why-choose-us-image/why-choose-us.jpg";
 import faqImage from "../../../assets/faq-image/faq-01.jpg";
 import blogImage from "../../../assets/aboutUs-image/aboutus-profile.jpg";
-import getFreeConsultationBackgroundImage from "../../../assets/get-a-free-consultation/bg-01.png";
-import getFreeConsultationImage from "../../../assets/get-a-free-consultation/img-01.jpg";
+
 import projectBackgroundImage from "../../../assets/background-effects/latest-projects-bg-effect.png";
 import projectImage from "../../../assets/latest-projects-image/projects-img-01.jpg";
 import teamMemberBackgroundImage from "../../../assets/background-effects/our-team-bg-effect.jpg";
 import teamMemberImage from "../../../assets/our-team-member-images/team-01.jpg";
+import construction from "../../../assets/construction.png";
 import Banner from "../../Header/Banner";
 import AboutUsSection from "../../AboutUsSection";
 import WorkProcessSection from "../../WorkProcessSection";
-import ServiceSection from "../../ServiceSection";
 import ProjectCompletationCount from "../../ProjectCompletationCount";
 import TestimonialSection from "../../TestimonialSection";
 import ContactUsSection from "../../ContactUsSection";
+import GetFreeConsultation from "../../GetFreeConsultation";
 
 export default function Home() {
-  const getFreeConsultationBackground = {
-    backgroundImage: `url(${getFreeConsultationBackgroundImage})`,
-  };
   const projectBackground = {
     backgroundImage: `url(${projectBackgroundImage})`,
   };
@@ -31,51 +34,134 @@ export default function Home() {
     <Fragment>
       <Banner />
       <AboutUsSection />
-      <ServiceSection />
-      <WorkProcessSection />
-      <div className="relative">
-        {/* get free consultation section */}
-        <div
-          style={getFreeConsultationBackground}
-          className=" bg-no-repeat bg-cover w-full  "
-        >
-          <div className="max-w-screen-xl mx-auto p-8 md:p-16 lg:p-8 ">
-            <div className="flex items-center justify-between gap-10">
-              {/* text  */}
-              <div className="space-y-3 md:space-y-4">
-                <div className="flex items-center gap-3">
-                  <h2 className="text-base font-[titillium] font-[600] text-[#F68A0A]">
-                    GET CONSULTATION
-                  </h2>
-                  <hr className="border-[1px] border-[#F68A0A] w-12" />
-                </div>
-                <h3 className="text-[36px] lg:text-[48px] text-white font-[titillium] font-[700]">
-                  Get A Free Consultation Contact Us !
-                </h3>
-                {/* contact us button  */}
-                <div>
-                  <button className="bg-[#F68A0A] hover:bg-[#ee8404] transform transition-all duration-300 px-5 lg:px-7 text-sm md:text-base font-[archivo] font-[600] py-3 lg:py-4 text-white uppercase">
-                    CONTACT US
-                  </button>
-                </div>
-              </div>
 
-              {/* image  */}
-              <div>
-                <img
-                  className="w-80 xl:w-96 h-60 xl:h-80 rounded-tl-[50%] 2xl:rounded-tl-[45%] rounded-bl-[50%] 2xl:rounded-bl-[45%] hidden lg:block"
-                  src={getFreeConsultationImage}
-                  alt=""
-                />
-              </div>
+      {/* service section */}
+      <div className="bg-[url('./assets/background-effects/bg-effect-service.png')] bg-no-repeat bg-cover w-full my-20">
+        <div className="max-w-screen-xl mx-auto p-8 md:p-16 lg:p-8">
+          {/* text  */}
+          <div className="space-y-3 md:space-y-4">
+            <div className="flex items-center gap-3 justify-center">
+              <hr className="border-[1px] border-[#F68A0A] w-12" />
+              <h2 className="text-base font-[titillium] font-[600] text-[#F68A0A]">
+                OUR SERVICES
+              </h2>
+              <hr className="border-[1px] border-[#F68A0A] w-12" />
+            </div>
+
+            <h3 className="text-[36px] lg:text-[48px] font-[titillium] font-[700] text-[#0E121D] text-center">
+              The Best Service For You
+            </h3>
+
+            <div className="flex justify-end">
+              <button className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:bg-[#F68A0A] px-3 py-2 hover:text-white transform transition-all duration-300 flex items-center gap-2 border-[1px] hover:border-transparent border-[#4D5765]">
+                VIEW ALL
+                <FaArrowRight className="text-sm" />
+              </button>
             </div>
           </div>
+
+          {/* start card  */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-3 md:mt-4 ">
+            {/* card 01  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button */}
+              <button className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2">
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </button>
+            </div>
+            {/* end card 01  */}
+            {/* card 02  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button */}
+              <button className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2">
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </button>
+            </div>
+            {/* end card 02  */}
+            {/* card 03  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button */}
+              <button className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2">
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </button>
+            </div>
+            {/* end card 03  */}
+          </div>
+          {/* end card  */}
         </div>
-
-       <div className="absolute top-[90%] lg:top-[85%] left-2/4 -translate-x-2/4 w-full">
-       <ProjectCompletationCount />
-       </div>
-
+      </div>
+      <WorkProcessSection />
+      <div className="relative">
+        <GetFreeConsultation />
+        <div className="absolute top-[90%] lg:top-[85%] left-2/4 -translate-x-2/4 w-full">
+          <ProjectCompletationCount />
+        </div>
       </div>
 
       {/* project section */}
--- src/components/Page/Service/Service.jsx
+++ src/components/Page/Service/Service.jsx
@@ -1,11 +1,238 @@
 import { Fragment } from "react";
+import CommonPageBanner from "../../CommonPageBanner";
+import construction from "../../../assets/construction.png";
+import { FaArrowRight } from "react-icons/fa";
+import GetFreeConsultation from "../../GetFreeConsultation";
+import TestimonialSection from "../../TestimonialSection";
 
-export default function Service(){
-    return(
-        <Fragment>
-            <div>
-                <p>Service page</p>
+export default function Service() {
+  return (
+    <Fragment>
+      <CommonPageBanner heading="Service" tittle="Home | Service" />
+      {/* service */}
+      <div className="bg-[url('./assets/background-effects/bg-effect-service.png')] bg-no-repeat bg-cover w-full my-20">
+        <div className="max-w-screen-xl mx-auto p-8 md:p-16 lg:p-8">
+          {/* text  */}
+          <div className="space-y-3 md:space-y-4">
+            <div className="flex items-center gap-3 justify-center">
+              <hr className="border-[1px] border-[#F68A0A] w-12" />
+              <h2 className="text-base font-[titillium] font-[600] text-[#F68A0A]">
+                OUR SERVICES
+              </h2>
+              <hr className="border-[1px] border-[#F68A0A] w-12" />
             </div>
-        </Fragment>
-    )
+
+            <h3 className="text-[36px] lg:text-[48px] font-[titillium] font-[700] text-[#0E121D] text-center">
+              The Best Service For You
+            </h3>
+          </div>
+
+          {/* start card  */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-3 md:mt-4 ">
+            {/* card 01  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button  */}
+              <a
+                href="./serviceDetails.html"
+                className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2"
+              >
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </a>
+            </div>
+            {/* end card 01  */}
+            {/* card 01  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button  */}
+              <a
+                href="./serviceDetails.html"
+                className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2"
+              >
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </a>
+            </div>
+            {/* end card 01  */}
+            {/* card 01  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button  */}
+              <a
+                href="./serviceDetails.html"
+                className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2"
+              >
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </a>
+            </div>
+            {/* end card 01  */}
+            {/* card 01  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button  */}
+              <a
+                href="./serviceDetails.html"
+                className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2"
+              >
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </a>
+            </div>
+            {/* end card 01  */}
+            {/* card 01  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button  */}
+              <a
+                href="./serviceDetails.html"
+                className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2"
+              >
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </a>
+            </div>
+            {/* end card 01  */}
+            {/* card 01  */}
+            <div className="bg-white p-6 space-y-3 md:space-y-4 xl:space-y-6">
+              <div className="flex items-center justify-between">
+                {/* icon  */}
+                <img className="w-12 h-12" src={construction} alt="" />
+                <span className="text-[20px] text-[#4D5765] font-[titillium] font-[600]">
+                  01
+                </span>
+              </div>
+
+              <div>
+                <hr className="border-[1px] border-[#E4E4E4]" />
+              </div>
+
+              {/* text  */}
+              <h2 className="font-[titillium] font-[600] text-[20px] lg:text-[24px] text-[#0E121D]">
+                Building Construction
+              </h2>
+              <p className="font-[archivo] text-base text-[#4D5765]">
+                {" "}
+                We offer comprehensive construction solutions, ensuring quality,
+                efficiency, and adherence to deadlines
+              </p>
+              {/* read more button  */}
+              <a
+                href="./serviceDetails.html"
+                className="uppercase text-sm font-[archivo] font-[600] text-[#0E121D] hover:text-[#F68A0A] transform transition-all duration-200 flex items-center gap-2"
+              >
+                READ MORE
+                <FaArrowRight className="text-sm" />
+              </a>
+            </div>
+            {/* end card 01  */}
+          </div>
+          {/* end card  */}
+        </div>
+      </div>
+      <GetFreeConsultation />
+      <TestimonialSection/>
+    </Fragment>
+  );
 }
