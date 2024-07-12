e25e3150e7bbca9f4ef36ec8aaeb6da2bee67f82
Moshfiqur Rahman <studentemam@gmail.com>
2024-07-12 15:44:22 +0600

    manage testimonial table skeleton

--- src/components/Page/DashboardPage/DashboardTestimonial/ManageTestimonial/ManageTestimonial.jsx
+++ src/components/Page/DashboardPage/DashboardTestimonial/ManageTestimonial/ManageTestimonial.jsx
@@ -6,6 +6,7 @@ import { FaPen } from "react-icons/fa";
 import { FaTrashCan } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import Delete from "./Action/Delete";
+import ManageTestimonialTableSkeleton from "../../../../skeleton/Dashboard/ManageTestimonialTableSkeleton";
 
 export default function ManageTestimonial({
   testimonialsData,
@@ -13,94 +14,98 @@ export default function ManageTestimonial({
 }) {
   return (
     <Fragment>
-      <WrapStyle>
-        <div className="max-w-screen-xl mx-auto ">
-          <h3 className="text-[#111111] font-[inter] font-bold text-2xl pb-4">
-            Manage Testimonial
-          </h3>
+      {testimonialLoading ? (
+        <ManageTestimonialTableSkeleton />
+      ) : (
+        <WrapStyle>
+          <div className="max-w-screen-xl mx-auto ">
+            <h3 className="text-[#111111] font-[inter] font-bold text-2xl pb-4">
+              Manage Testimonial
+            </h3>
 
-          <div className="p-5 w-full  h-auto bg-white rounded-xl ">
-            <div className="flex flex-col md:flex-row gap-3 md:items-center md:justify-between md:mb-5">
-              <Searchbar />
+            <div className="p-5 w-full  h-auto bg-white rounded-xl ">
+              <div className="flex flex-col md:flex-row gap-3 md:items-center md:justify-between md:mb-5">
+                <Searchbar />
 
-              <Link to="/dashboard/addTestimonial">
-                <AddButton button="Add Testimonial" />
-              </Link>
-            </div>
+                <Link to="/dashboard/addTestimonial">
+                  <AddButton button="Add Testimonial" />
+                </Link>
+              </div>
 
-            <div className="overflow-auto">
-              <table className="w-full border-separate border-spacing-y-3 ">
-                <thead>
-                  <tr className="bg-[#F8F9FC] ">
-                    <td className="text-[#111111] font-[inter] font-[700] text-base p-3   whitespace-nowrap">
-                      Image
-                    </td>
-                    <td className="text-[#111111] font-[inter] font-[700] text-base  p-3  whitespace-nowrap">
-                      Testimonial Person Name
-                    </td>
-                    <td className="text-[#111111] font-[inter] font-[700] text-base  p-3  whitespace-nowrap">
-                      Review
-                    </td>
-                    <td className="text-[#111111] font-[inter] font-[700] text-base  p-3  whitespace-nowrap">
-                      Ratings
-                    </td>
-                    <td className="text-[#111111] font-[inter] font-[700]  text-base  p-3  whitespace-nowrap">
-                      Action
-                    </td>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  {testimonialsData?.map((testimonial, _id) => (
-                    <tr
-                      key={_id}
-                      className="bg-white hover:bg-[#EEF3F7] odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200"
-                    >
-                      <td className="align-middle p-3 ">
-                        <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
-                          <img
-                            className="w-10 h-12 object-cover rounded-lg"
-                            src={testimonial?.imageUrl}
-                            alt={testimonial?.name + " " + "Image"}
-                          />
-                        </div>
+              <div className="overflow-auto">
+                <table className="w-full border-separate border-spacing-y-3 ">
+                  <thead>
+                    <tr className="bg-[#F8F9FC] ">
+                      <td className="text-[#111111] font-[inter] font-[700] text-base p-3   whitespace-nowrap">
+                        Image
                       </td>
-
-                      <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573] ">
-                        {testimonial?.name}
+                      <td className="text-[#111111] font-[inter] font-[700] text-base  p-3  whitespace-nowrap">
+                        Testimonial Person Name
                       </td>
-
-                      <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573] ">
-                        {testimonial?.review}
+                      <td className="text-[#111111] font-[inter] font-[700] text-base  p-3  whitespace-nowrap">
+                        Review
                       </td>
-
-                      <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573] ">
-                        {testimonial?.star}
+                      <td className="text-[#111111] font-[inter] font-[700] text-base  p-3  whitespace-nowrap">
+                        Ratings
                       </td>
-
-                      <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                        <div className="flex  gap-3">
-                          <button>
-                            <span className="text-[#22C7A1] font-extrabold">
-                              <Link
-                                to={`/dashboard/updateTestimonial/${testimonial._id}`}
-                              >
-                                <FaPen />
-                              </Link>
-                            </span>
-                          </button>
-
-                          <Delete id={testimonial._id} />
-                        </div>
+                      <td className="text-[#111111] font-[inter] font-[700]  text-base  p-3  whitespace-nowrap">
+                        Action
                       </td>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+
+                  <tbody>
+                    {testimonialsData?.map((testimonial, _id) => (
+                      <tr
+                        key={_id}
+                        className="bg-white hover:bg-[#EEF3F7] odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200"
+                      >
+                        <td className="align-middle p-3 ">
+                          <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
+                            <img
+                              className="w-10 h-12 object-cover rounded-lg"
+                              src={testimonial?.imageUrl}
+                              alt={testimonial?.name + " " + "Image"}
+                            />
+                          </div>
+                        </td>
+
+                        <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573] ">
+                          {testimonial?.name}
+                        </td>
+
+                        <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573] ">
+                          {testimonial?.review}
+                        </td>
+
+                        <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573] ">
+                          {testimonial?.star}
+                        </td>
+
+                        <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
+                          <div className="flex  gap-3">
+                            <button>
+                              <span className="text-[#22C7A1] font-extrabold">
+                                <Link
+                                  to={`/dashboard/updateTestimonial/${testimonial._id}`}
+                                >
+                                  <FaPen />
+                                </Link>
+                              </span>
+                            </button>
+
+                            <Delete id={testimonial._id} />
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-      </WrapStyle>
+        </WrapStyle>
+      )}
     </Fragment>
   );
 }
--- src/components/skeleton/Dashboard/ManageTestimonialTableSkeleton.jsx
+++ src/components/skeleton/Dashboard/ManageTestimonialTableSkeleton.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
 import WrapStyle from "../../Dashboard/WrapStyle";
 
-export default function ManageProjectTableSkeleton() {
+export default function ManageTestimonialTableSkeleton() {
   return (
     <Fragment>
       <WrapStyle>
         <div className="max-w-screen-xl mx-auto">
           <h3 className="text-[#111111] font-[inter] font-bold text-2xl pb-4">
-            Manage Project
+            Manage Testimonial
           </h3>
 
           <div className="p-5 w-full h-auto bg-white rounded-xl">
@@ -26,7 +26,16 @@ export default function ManageProjectTableSkeleton() {
                     <td className="text-[#111111] font-[inter] font-[700] text-base p-3 whitespace-nowrap">
                       Image
                     </td>
-                    <td className="text-[#111111] font-[inter] font-[700] text-end text-base p-3 whitespace-nowrap">
+                    <td className="text-[#111111] font-[inter] font-[700] text-base p-3 whitespace-nowrap">
+                      Testimonial Person Name
+                    </td>
+                    <td className="text-[#111111] font-[inter] font-[700] text-base p-3 whitespace-nowrap">
+                      Review
+                    </td>
+                    <td className="text-[#111111] font-[inter] font-[700] text-base p-3 whitespace-nowrap">
+                      Ratings
+                    </td>
+                    <td className="text-[#111111] font-[inter] font-[700] text-base p-3 whitespace-nowrap">
                       Action
                     </td>
                   </tr>
@@ -40,14 +49,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -55,14 +75,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -70,14 +101,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -85,29 +127,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
-                  <tr className="bg-white odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
-                    <td className="align-middle p-3">
-                      <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
-                        <div className="w-10 h-12 bg-white rounded-lg animate-pulse"></div>
-                      </div>
-                    </td>
-
-                    <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
-                        <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
-                        <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
-                      </div>
-                    </td>
-                  </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -115,14 +153,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -130,14 +179,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -145,14 +205,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -160,14 +231,25 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
                     </td>
                   </tr>
-
                   <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
                     <td className="align-middle p-3">
                       <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
@@ -175,8 +257,46 @@ export default function ManageProjectTableSkeleton() {
                       </div>
                     </td>
 
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
                     <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
-                      <div className="flex justify-end gap-3">
+                      <div className="flex gap-3">
+                        <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
+                        <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr className="bg-white  odd:bg-[#F8F9FC] cursor-default transform transition-all duration-200">
+                    <td className="align-middle p-3">
+                      <div className="w-12 h-14 bg-[#EFF4F8] flex items-center justify-center rounded-xl">
+                        <div className="w-10 h-12 bg-white rounded-lg animate-pulse"></div>
+                      </div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-20 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-normal text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-36 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-base font-[inter] font-normal whitespace-nowrap text-[#5C6573]">
+                      <div className="bg-[#EFF4F8] w-12 h-4 animate-pulse rounded"></div>
+                    </td>
+
+                    <td className="p-3 align-middle text-lg font-[inter] whitespace-nowrap">
+                      <div className="flex gap-3">
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                         <div className="w-8 h-8 bg-[#EFF4F8] rounded-full animate-pulse"></div>
                       </div>
